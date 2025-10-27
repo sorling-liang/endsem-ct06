89406ce13bcfccc7be2ffc11f06ed5816e590c6a
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-10-27 10:55:53 +0000

    Oct 27, 2025, 10:55 AM

--- testquestion1.js
+++ testquestion1.js
@@ -4,11 +4,12 @@ let speed;
 
 
 function setup() {
-    createCanvas(400, 600);
+    createCanvas(600, 400);
     background("lightblue");
     speed = 5;
 }
 
 function draw() {
+    fill("limegreen");
     circle(50, 50, 50);
 }
