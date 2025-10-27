f7d6387d6e534371c5b1156b6faae8c2c3bc1f69
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-10-27 11:01:43 +0000

    Oct 27, 2025, 11:01 AM

--- testquestion2.js
+++ testquestion2.js
@@ -6,8 +6,10 @@ function setup() {
     textSize(20);
     textAlign(CENTER, CENTER);
     fill("pink");
+    let ypos = 50;
 
     for ( let index=0; index< pokemons.length; index++) {
-        text( pokemons[index], width/2, 50);
+        text( pokemons[index], width/2, ypos);
+        ypos = ypos + 35;
     }
 }
